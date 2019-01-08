f55f8dea5b47b29751027addb2e3d050598f68b7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-01-08 17:05:28 -0200

    few changes to allow call methods using a telegram bot

--- make_timelapse.py
+++ make_timelapse.py
@@ -5,62 +5,79 @@ import os
 import shutil
 import sys
 import cli_utils
+import numpy as np
 
-# Check arguments for fps
-output_fps = float(cli_utils.get_cli_arg_with_default("--fps", 5.0))
+def make(today, output_fps):
 
-# Check arguments for custom date
-custom_date = cli_utils.get_cli_arg("--date")
-if custom_date is None and cli_utils.has_cli_arg("--today"):
-    custom_date = date.today().strftime("%Y-%m-%d")
+    # Check arguments for custom date
+    custom_date = cli_utils.get_cli_arg("--date")
+    if custom_date is None and today:
+        custom_date = date.today().strftime("%Y-%m-%d")
 
-if custom_date is None:
-    # Get yesterday's snapshots
-    yesterday = date.today() - timedelta(1)
-    source_date = yesterday.strftime("%Y-%m-%d")
-else:
-    # Get snapshots for specified date
-    source_date = custom_date
+    if custom_date is None:
+        # Get yesterday's snapshots
+        yesterday = date.today() - timedelta(1)
+        source_date = yesterday.strftime("%Y-%m-%d")
+    else:
+        # Get snapshots for specified date
+        source_date = custom_date
 
-# Determine source directory
-absolute_script_dir = os.path.dirname(os.path.realpath(__file__))
-source_dir = absolute_script_dir + '/snapshots/' + source_date
+    # Determine source directory
+    absolute_script_dir = os.path.dirname(os.path.realpath(__file__))
+    source_dir = absolute_script_dir + '/snapshots/' + source_date
 
-if not os.path.exists(source_dir):
-    print("Error: Source directory does not exist: {}".format(source_dir))
-    sys.exit(1)
+    if not os.path.exists(source_dir):
+        print("Error: Source directory does not exist: {}".format(source_dir))
+        sys.exit(1)
 
-# Determine save location for timelapse video
-save_dir = absolute_script_dir + '/timelapses'
-save_path = save_dir + '/' + source_date + '.avi'
+    # Determine save location for timelapse video
+    save_dir = absolute_script_dir + '/timelapses'
+    save_path = save_dir + '/' + source_date + '.mp4'
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-# Delete any empty image files that were created in error
-for imageFile in os.listdir(source_dir):
-    if os.path.getsize(source_dir + "/" + imageFile) == 0:
-        os.remove(source_dir + "/" + imageFile)
+    # Delete any empty image files that were created in error
+    for imageFile in os.listdir(source_dir):
+        if os.path.getsize(source_dir + "/" + imageFile) == 0:
+            os.remove(source_dir + "/" + imageFile)
 
-# Create timelapse and save
-images = [img for img in os.listdir(source_dir)]
-images.sort()
-frame = cv2.imread(os.path.join(source_dir, images[0]))
-height, width, layers = frame.shape
+    # Create timelapse and save
+    images = [img for img in os.listdir(source_dir) if not img.startswith('.') ]
+    images.sort()
+    frame = cv2.imread(source_dir+ "/"+images[0])
+    height, width, layers = frame.shape
 
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-video = cv2.VideoWriter(save_path, fourcc, output_fps, (width,height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(save_path, fourcc, output_fps, (width, height ))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(source_dir, image)))
+    for image in images:
+        img = cv2.imread(os.path.join(source_dir, image))
 
-video.release()
+        # Shape of image in terms of pixels.
+        # (rows, cols) = img.shape[:2]
+        # M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -90, 1)
+        # res = imutils.rotate(img, -90)
+        # res = cv2.warpAffine(img, M, (cols, rows))
+        # src = cv2.transpose(cv2.imread(os.path.join(source_dir, image)))
+        # src = cv2.flip(src,flipCode=0)
+        video.write(img)
 
-# Ensure timelapse was generated successfully
-if not os.path.exists(save_path):
-    print("Error: Failed to generate timelapse video file.")
-    sys.exit(2)
+    video.release()
 
-# Delete source images if desired
-if cli_utils.has_cli_arg("--cleanup"):
-    shutil.rmtree(source_dir)
+    # Ensure timelapse was generated successfully
+    if not os.path.exists(save_path):
+        print("Error: Failed to generate timelapse video file.")
+        sys.exit(2)
+
+    # Delete source images if desired
+    if cli_utils.has_cli_arg("--cleanup"):
+        shutil.rmtree(source_dir)
+    print(save_path)
+    return save_path
+def main():
+    print(make(today=cli_utils.has_cli_arg("--today"), output_fps = float(cli_utils.get_cli_arg_with_default("--fps", 5.0))))
+
+
+if __name__ == "__main__":
+    main()
--- take_snapshot.py
+++ take_snapshot.py
@@ -4,22 +4,32 @@ import cv2
 import os
 
 # Config
-stream_url = 'http://192.168.1.2:4321/stream.mjpg'
+stream_url = 'http://192.168.0.11:8080/video'
 
-# Determine save location
-now = datetime.datetime.now()
-date = now.strftime("%Y-%m-%d")
-time = now.strftime("%H-%M-%S")
-absolute_script_dir = os.path.dirname(os.path.realpath(__file__))
-save_dir = absolute_script_dir + '/snapshots/' + date
-save_path = save_dir + '/' + time + '.jpg'
+def take():
+    # Determine save location
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d")
+    time = now.strftime("%H-%M-%S")
+    absolute_script_dir = os.path.dirname(os.path.realpath(__file__))
+    save_dir = absolute_script_dir + '/snapshots/' + date
+    save_path = save_dir + '/' + time + '.jpg'
 
-# Capture frame from camera stream
-cap = cv2.VideoCapture(stream_url)
-ret, frame = cap.read()
+    # Capture frame from camera stream
+    cap = cv2.VideoCapture(stream_url)
+    ret, frame = cap.read()
 
-# Save frame as image
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+    # Save frame as image
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-cv2.imwrite(save_path, frame)
+    cv2.imwrite(save_path, frame)
+    return save_path
+    # return '2019-01-08/02-54-01.jpg'
+
+def main():
+    print(take())
+
+
+if __name__ == "__main__":
+    main()
